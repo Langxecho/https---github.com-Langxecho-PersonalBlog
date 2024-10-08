31ec5cf6cff88667869671fc2f411499021e68d7
庆宇 王 <1908630172@qq.com>
2024-10-08 09:35:05 +0800

    项目创建

---
+++ docs/.vitepress/index.ts
@@ -0,0 +1,10 @@
+import DefaultTheme from 'vitepress/theme';
+import 'vitepress-theme-demoblock/dist/theme/styles/index.css';
+import './styles/var.css';
+
+export default {
+  ...DefaultTheme,
+  enhanceApp({ app }) {
+    // 代码片段在这里结束，后续的代码没有提供
+  }
+};
